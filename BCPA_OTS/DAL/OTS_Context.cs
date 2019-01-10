c2f8a063bd2c8e9b2bf2b99f2bb0ea031d4343ab
Connor Patey <pateyco@hotmail.com>
2019-01-10 13:54:11 +0000

    Added Context

---
+++ BCPA_OTS/DAL/OTS_Context.cs
@@ -0,0 +1,22 @@
+﻿using BCPA_OTS.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BCPA_OTS.DAL
+{
+    public class OTS_Context : DbContext
+    {
+        public OTS_Context() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Person> People { get; set; }
+
+        public DbSet<Ticket> Tickets { get; set; }
+
+        public DbSet<PaymentCard> PaymentCards { get; set; }
+    }
+}
